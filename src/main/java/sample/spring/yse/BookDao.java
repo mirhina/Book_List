0a15a11d6c1f2586829696fc0d14edf1cf5b92b4
mirhina <wjstkdgns96@naver.com>
2023-03-31 11:15:30 +0900

    Third commit
    - 책 수정 페이지 디자인 및 기능 구현
    - 책 삭제 기능 구현
    - 책 목록 페이지 디자인 및 기능 구현
    - 책 검색 기능 추가

--- src/main/java/sample/spring/yse/BookDao.java
+++ src/main/java/sample/spring/yse/BookDao.java
@@ -1,5 +1,6 @@
 package sample.spring.yse;
 
+import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -14,4 +15,20 @@ public class BookDao {
  public int insert(Map<String, Object> map) {
 	  return this.sqlSessionTemplate.insert("book.insert", map);
 	}
+
+ public Map<String, Object> selectDetail(Map<String, Object> map) {
+	 return this.sqlSessionTemplate.selectOne("book.select_detail", map);
+ }
+ 
+ public int update(Map<String, Object> map) {
+	 return this.sqlSessionTemplate.update("book.update", map);
+ }
+ 
+ public int delete(Map<String, Object> map) {
+	 return this.sqlSessionTemplate.delete("book.delete", map);
+ }
+ 
+ public List<Map<String, Object>> selectList(Map<String, Object> map) {
+	 return this.sqlSessionTemplate.selectList("book.select_list", map);
+ }
 }
